bec9ffa690d4b40ffddbd0044a655e4ec04f0037
Chaoticwave-boop <bren.stoute@hotmail.com>
2023-03-31 09:44:33 +0200

    madlabs validation completed

--- madlibsstart.php
+++ madlibsstart.php
@@ -1,68 +1,172 @@
-<!DOCTYPE html>
+<?php
+    $error = [];
 
-<html lang="en">
+    if ($_SERVER['REQUEST_METHOD'] == "POST"){
+
+        $dier =  $_POST["fdier"];
+        $belangrijkpersoon =  $_POST["fbelangrijkpersoon"];
+        $land =  $_POST["fland"];
+        $verveeld =  $_POST["fverveeld"];
+        $speelgoed =  $_POST["fspeelgoed"];
+        $docent =  $_POST["docent"];
+        $geld =  $_POST["fgeld"];
+        $bezigheid =  $_POST["fbezigheid"];
+
+        foreach($_POST as $key => $value){
+            if (!$value) {
+                $error[$key] = "$key is niet ingevuld";
+            }
+        };
+    } else {
+       
+    };
+?>
+
+<!DOCTYPE html>
+<html>
     <head>
         <link  rel="stylesheet" type="text/css" href="madlibls.css"/>
-        <title>Document</title>
     </head>
 
     <body>
-        <h1>Mad Libs</h1>
+        <?php  if ($_SERVER['REQUEST_METHOD'] == "GET" || !empty($error)) { ?>
+            <link  rel="stylesheet" type="text/css" href="madlibls.css"/>
+            <title>Document</title>
         
-        <div id="topform">
-            <a href="madlibsstart.php">er heerst paniek</a>
-            <a href="onkundestart.php">onkunde</a>
-        </div>
+            <h1>Mad Libs</h1>
+                
+            <div id="topform">
+                <a href="madlibsstart.php">er heerst paniek</a>
+                <a href="onkundestart.php">onkunde</a>
+            </div>
+    
+            <form action="madlibsstart.php" method="post" id="form">
+                <div>
+                    <h2>er heerst paniek</h2>
+                </div>
+                <div>
+                    <p>welk dier zou je nooit als huisdier willen hebben</p>
+                    <input  name="fdier">
+                    <?php 
+                        if (array_key_exists('fdier', $error)) { ?>
+                            <p id="error">je hebt dit veld niet ingevuld </p>
+                        <?php }; 
+                    ?>
+                </div>
+    
+                <div>
+                    <p>wie is de belangrijkste persoon in je leven</p>
+                    <input  name="fbelangrijkpersoon">
+                    <?php 
+                        if (array_key_exists('fbelangrijkpersoon', $error)) { ?>
+                            <p id="error">je hebt dit veld niet ingevuld </p>
+                        <?php }; 
+                    ?>
+                </div>
 
-        <form action="madlibs.php" method="post" id="form">
-            <div>
-                <h2>er heerst paniek</h2>
-            </div>
-            <div>
-                <p>welk dier zou je nooit als huisdier willen hebben</p>
-                <input  name="fdier">
-            </div>
-
-            <div>
-                <p>wie is de belangrijkste persoon in je leven</p>
-                <input  name="fbelangrijkpersoon">
-            </div>
-
-            <div>
-                <p>In welk land zou je graag willen wonen</p>
-                <input  name="fland">
-            </div>
-
-            <div>
-                <p>Wat doe je als je je verveeld</p>
-                <input  name="fverveeld">
-            </div>
-
-            <div>
-                <p>met welk speelgoed speelde ja als kind het meest?</p>
-                <input  name="fspeelgoed">
-            </div>
-
-            <div>
-                <p>bij welke docent spijbel je het liefst?</p>
-                <input  name="docent">
-            </div>
-
-            <div>
-                <p>als je 100.000 euro had, wat zou je dan kopen?</p>
-                <input  name="fgeld">
-            </div>
-
-            <div>
-                <p>wat is je favoriete bezigheid</p>
-                <input  name="fbezigheid">
-            </div>
+                <div>
+                    <p>wat was je favoriete speelgoed als kind?</p>
+                    <input  name="fspeelgoed">
+                    <?php 
+                        if (array_key_exists('fspeelgoed', $error)) { ?>
+                            <p id="error">je hebt dit veld niet ingevuld </p>
+                        <?php }; 
+                    ?>
+                </div>
+    
+                <div>
+                    <p>In welk land zou je graag willen wonen</p>
+                    <input  name="fland">
+                    <?php 
+                        if (array_key_exists('fland', $error)) { ?>
+                            <p id="error">je hebt dit veld niet ingevuld </p>
+                        <?php }; 
+                    ?>
+                </div>
+    
+                <div>
+                    <p>Wat doe je als je je verveeld</p>
+                    <input  name="fverveeld">
+                    <?php 
+                        if (array_key_exists('fverveeld', $error)) { ?>
+                            <p id="error">je hebt dit veld niet ingevuld </p>
+                        <?php }; 
+                    ?>
+                </div>
+    
+                <div>
+                    <p>bij welke docent spijbel je het liefst?</p>
+                    <input  name="docent">
+                    <?php 
+                        if (array_key_exists('docent', $error)) { ?>
+                            <p id="error">je hebt dit veld niet ingevuld </p>
+                        <?php }; 
+                    ?>
+                </div>
+    
+                <div>
+                    <p>als je 100.000 euro had, wat zou je dan kopen?</p>
+                    <input  name="fgeld">
+                    <?php 
+                        if (array_key_exists('fgeld', $error)) { ?>
+                            <p id="error">je hebt dit veld niet ingevuld </p>
+                        <?php }; 
+                    ?>
+                </div>
+    
+                <div>
+                    <p>wat is je favoriete bezigheid</p>
+                    <input  name="fbezigheid">
+                    <?php 
+                        if (array_key_exists('fbezigheid', $error)) { ?>
+                            <p id="error">je hebt dit veld niet ingevuld </p>
+                        <?php }; 
+                    ?>
+                </div>
+    
+                <div>
+                    <input type="submit"> 
+                </div>
+    
+            </form>
+        <?php
+        }  else { ?>
+            <h1>Mad Libs</h1>
             
-            <div>
-                <input type="submit"> 
+            <div id="topform">
+                <a href="madlibsstart.php">er heerst paniek</a>
+                <a href="onkundestart.php">onkunde</a>
             </div>
 
-        </form>
+        
+            <div id="formanswer">
+                <h2>Onkunde</h2>
+                <br> 
+                <?php 
+                echo "<h3>
+                er heerst paniek in het koningkrijk $land koning $docent is ten einde raad en als 
+                koning $docent ten einde raad is, dan roept hij zijn ten-einde-raadsheer $belangrijkpersoon
+                <br>
+                ' $belangrijkpersoon het is een ramp ! het is een schande ! '
+                <br>
+                ' sire, majesteit, uwe leidruchtingheid , was is er aan de hand? '
+                <br>
+                ' mijn $dier is verdwenen! zo maar, zonder waarschuuwing. en ik had net $speelgoed voor hem gekocht '
+                <br>
+                ' majesteit uw $dier komt vast vanzelf wel weer terug?'
+                <br>
+                ' ja, das leuk en aardig, maar hoe moet ik in de tussentijds $bezigheid leren 
+                <br>'
+                ' maar sire, daar kun uw de $geld voor gebruiken '
+                <br>
+                ' $belangrijkpersoon uw hebt helemaal gelijk, wat zou ik doen als ik jouw niet had '
+                <br>
+                ' $verveeld, Sire '
 
+                   
+                </h3>";
+            ?>
+            </div>
+        <?php } ?> 
     </body>
 </html>
--- onkundestart.php
+++ onkundestart.php
@@ -1,63 +1,145 @@
-<!DOCTYPE html>
+<?php
+    $error = [];
 
-<html lang="en">
+    if ($_SERVER['REQUEST_METHOD'] == "POST"){
+
+        $kunnen =  $_POST["fkunnen"];
+        $persoon =  $_POST["fpersoon"];
+        $getal =  $_POST["fgetal"];
+        $vakantie =  $_POST["fvakantie"];
+        $eigenschapgoed =  $_POST["feigenschapgoed"];
+        $eigenschapslecht =  $_POST["feigenschapslecht"];
+        $overkomen =  $_POST["foverkomen"];
+
+        foreach($_POST as $key => $value){
+            if (!$value) {
+                $error[$key] = "$key is niet ingevuld";
+            }
+        };
+    } else {
+        
+    };
+?>
+
+<!DOCTYPE html>
+<html>
     <head>
         <link  rel="stylesheet" type="text/css" href="madlibls.css"/>
-        <title>Document</title>
     </head>
 
     <body>
-        <h1>Mad Libs</h1>
+        <?php  if ($_SERVER['REQUEST_METHOD'] == "GET" || !empty($error)) { ?>
+            <link  rel="stylesheet" type="text/css" href="madlibls.css"/>
+            <title>Document</title>
         
-        <div id="topform">
-            <a href="madlibsstart.php">er heerst paniek</a>
-            <a href="onkundestart.php">onkunde</a>
-        </div>
-
-        <form action="onkunde.php" method="post" id="form">
-            <div>
-                <h2>onkunde</h2>
+            <h1>Mad Libs</h1>
+                
+            <div id="topform">
+                <a href="madlibsstart.php">er heerst paniek</a>
+                <a href="onkundestart.php">onkunde</a>
             </div>
-            <div>
-                <p>wat zou je graag willen kunnen?</p>
-                <input  name="fkunnen">
+    
+            <form action="onkundestart.php" method="post" id="form">
+                <div>
+                    <h2>onkunde</h2>
+                </div>
+                <div>
+                    <p>wat zou je graag willen kunnen?</p>
+                    <input  name="fkunnen">
+                    <?php 
+                        if (array_key_exists('fkunnen', $error)) { ?>
+                            <p id="error">je hebt dit veld niet ingevuld </p>
+                        <?php }; 
+                    ?>
+                </div>
+    
+                <div>
+                    <p>met welk persoon kun je goed opschieten?</p>
+                    <input name="fpersoon">
+                    <?php 
+                        if (array_key_exists('fpersoon', $error)) { ?>
+                            <p id="error">je hebt dit veld niet ingevuld </p>
+                        <?php }; 
+                    ?>
+                </div>
+    
+                <div>
+                    <p>wat is je favoriete getal?</p>
+                    <input  name="fgetal">
+                    <?php 
+                        if (array_key_exists('fgetal', $error)) { ?>
+                            <p id="error">je hebt dit veld niet ingevuld </p>
+                        <?php }; 
+                    ?>
+                </div>
+    
+                <div>
+                    <p>wat heb je altijd bij je als je op vakantie gaat?</p>
+                    <input  name="fvakantie">
+                    <?php 
+                        if (array_key_exists('fvakantie', $error)) { ?>
+                            <p id="error">je hebt dit veld niet ingevuld </p>
+                        <?php }; 
+                    ?>
+                </div>
+    
+                <div>
+                    <p>wat is je beste persoonlijke eigenschap?</p>
+                    <input  name="feigenschapgoed">
+                    <?php 
+                        if (array_key_exists('feigenschapgoed', $error)) { ?>
+                            <p id="error">je hebt dit veld niet ingevuld </p>
+                        <?php }; 
+                    ?>
+                </div>
+    
+                <div>
+                    <p>wat is je slechtste persoonlijke eigenschap?</p>
+                    <input  name="feigenschapslecht">
+                    <?php 
+                        if (array_key_exists('feigenschapslecht', $error)) { ?>
+                            <p id="error">je hebt dit veld niet ingevuld </p>
+                        <?php }; 
+                    ?>
+                </div>
+    
+                <div>
+                    <p>wat is het ergste wat je kan overkomen</p>
+                    <input  name="foverkomen">
+                    <?php 
+                        if (array_key_exists('foverkomen', $error)) { ?>
+                            <p id="error">je hebt dit veld niet ingevuld </p>
+                        <?php }; 
+                    ?>
+                </div>
+    
+                <div>
+                    <input type="submit"> 
+                </div>
+    
+            </form>
+        <?php
+        }  else { ?>
+            <h1>Mad Libs</h1>
+            
+            <div id="topform">
+                <a href="madlibsstart.php">er heerst paniek</a>
+                <a href="onkundestart.php">onkunde</a>
             </div>
 
-            <div>
-                <p>met welk persoon kun je goed opschieten?</p>
-                <input name="fpersoon">
+        
+            <div id="formanswer">
+                <h2>Onkunde</h2>
+                <br> 
+                <?php 
+                    echo "<h3>
+                        er zijn veel mensen die niet kunnen $kunnen neem nou  $persoon zelfs met de hulp van een $vakantie
+                        of zelfs $getal kan $persoon niet $kunnen . dat heeft niks te maken met gebrek aan $eigenschapgoed ,
+                        maar met een te veel aan $eigenschapslecht. te veel $eigenschapslecht leidt tot $overkomen en dat is niet goed
+                        als je wilt $kunnen. helaas voor $persoon
+                    </h3>";
+                ?>
             </div>
-
-            <div>
-                <p>wat is je favoriete getal?</p>
-                <input  name="fgetal">
-            </div>
-
-            <div>
-                <p>wat heb je altijd bij je als je op vakantie gaat?</p>
-                <input  name="fvakantie">
-            </div>
-
-            <div>
-                <p>wat is je beste persoonlijke eigenschap?</p>
-                <input  name="feigenschapgoed">
-            </div>
-
-            <div>
-                <p>wat is je slechtste persoonlijke eigenschap?</p>
-                <input  name="feigenschapslecht">
-            </div>
-
-            <div>
-                <p>wat is het ergste wat je kan overkomen</p>
-                <input  name="foverkomen">
-            </div>
-
-           <div>
-                <input type="submit"> 
-           </div>
-
-        </form>
-
+        <?php } ?> 
     </body>
 </html>
